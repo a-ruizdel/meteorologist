b543c2924c97d62ab9089928431259a02e6e6782
a-ruizdel <a-rdl@Arlenes-MacBook-Air.local>
2015-07-21 22:47:22 -0500

    Completes Week 4 homework

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,23 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    url = "https://api.forecast.io/forecast/41f73306c5622f30ce59b676f5f07ed9/#{@lat},#{@lng}"
+    parsed_data = JSON.parse(open(url).read)
+    temperature = parsed_data["currently"]["temperature"]
+    summary = parsed_data["currently"]["summary"]
+    summary_sixty = parsed_data["minutely"]["summary"]
+    summary_hours = parsed_data["hourly"]["summary"]
+    summary_daily = parsed_data["daily"]["summary"]
 
+    @current_temperature = temperature
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = summary
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = summary_sixty
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = summary_hours
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = summary_daily
 
     render("coords_to_weather.html.erb")
   end
--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -16,12 +16,11 @@ class GeocodingController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
-require 'open-uri'
-url= "http://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}"
 
-parsed_data = JSON.parse(open(url).read)
-latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    url= "http://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}"
+    parsed_data = JSON.parse(open(url).read)
+    latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     @latitude = latitude
     @longitude = longitude
