c496105969d600cfd9aba50184a2e684fbf8d854
a-ruizdel <a-rdl@Arlenes-MacBook-Air.local>
2015-08-13 13:11:18 -0500

    Corrects URL safe street address

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,7 +17,7 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
-    url= "http://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}"
+    url= "http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
     parsed_data = JSON.parse(open(url).read)
     @lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
     @lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
